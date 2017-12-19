bb485e329f432d1a715e45ad736eb60ab472d645
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 21:52:43 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -8,8 +8,6 @@ id INT NOT NULL IDENTITY(1,1),
 PRIMARY KEY(id),
 name VARCHAR(30),
 email VARCHAR(30),
-gender VARCHAR(10),
-age VARCHAR(2),
 country VARCHAR(10),
 birthday VARCHAR(10),
 date DATE)";
