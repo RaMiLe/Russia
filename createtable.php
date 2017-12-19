20835ef22f1eac4f287d9699a12b502fbe99df3f
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 21:43:00 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,19 +1,24 @@
 <?php
+
 try {
-$conn = new PDO("sqlsrv:server = tcp:rom.database.windows.net,1433; Database = Ttt", "rom", "Rosbank1997");
+  $conn = new PDO("sqlsrv:server = tcp:rom.database.windows.net,1433; Database = Ttt", "rom", "Rosbank1997");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "CREATE TABLE registration_too(
-id INT NOT NULL IDENTITY(1,1)
+$sql = "CREATE TABLE registration_on(
+id INT NOT NULL IDENTITY(1,1),
 PRIMARY KEY(id),
 name VARCHAR(30),
 email VARCHAR(30),
-city VARCHAR(30),
+gender VARCHAR(10),
+age VARCHAR(2),
+country VARCHAR(10),
+birthday VARCHAR(10),
 date DATE)";
 $conn->query($sql);
 
-echo "<h3>Таблица создана!</h3>";
+echo "<h3>Таблица создана.</h3>";
 }
 catch (PDOException $e) {
-print("Error connecting to SQL Server.");
+print("Ошибка подключения к SQL Server.");
 die(print_r($e));
 }
+?>
